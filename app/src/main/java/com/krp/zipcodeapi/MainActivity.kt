2e1d449fe812dc6c183737c41db04cfb5349f0ad
Rakesh Konduru <rakeshpraneeth@gmail.com>
2021-12-01 20:03:54 -0500

    Created UI to provide zipcode and radius and search for the nearby.

--- app/src/main/java/com/krp/zipcodeapi/MainActivity.kt
+++ app/src/main/java/com/krp/zipcodeapi/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.krp.zipcodeapi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.krp.zipcodeapi.zipcodebyradius.ui.ZipcodeByRadiusActivity
 
+/**
+ * [MainActivity]
+ * Launcher activity and will be called when user opens/launches the app.
+ * When user launches, we are redirecting the user to [ZipcodeByRadiusActivity] to display the feature.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        startActivity(Intent(this, ZipcodeByRadiusActivity::class.java))
+        finish()
     }
 }
